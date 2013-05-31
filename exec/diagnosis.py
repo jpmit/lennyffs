c03c8ddbcd3bf8116d5dc9e7c5fff9a8a1e38ff4
James Mithen <j.mithen@surrey.ac.uk>
2013-05-31 16:02:34 +0100

    added variance in rate to diagnosis script

--- exec/diagnosis.py
+++ exec/diagnosis.py
@@ -2,6 +2,9 @@
 # diagnosis.py
 # 26th July 2012
 # analyse data at all ffs interfaces and write statistics out to file
+# we write two files, 'allinterfaces.out' and 'allinterfaceseff.out'
+# the latter file includes pruning, so if pruning is applied then
+# the results contained in 'allinterfaceseff.out' are the correct ones
 
 import readwrite
 import numpy as np
@@ -60,49 +63,63 @@ for i in range(nint):
     if cumprobseff[i] > 0.5:
         critinteff = i
         break
-    
+
 # critical OP is OP at the critical interface
 critop = params['lambdas'][critint]
 critopeff = params['lambdas'][critinteff]
 
-# write all data out to file (this file is for debug, see 'allinterfaceseff.out')
+# get 'relative variance' of rate
+# For this we use Equation (20) from:
+# R.J. Allen, D. Frenkel and P ten Wolde J. Chem. Phys. 124 194111 (2006)
+# a sensible estimate of the error in the rate from a single FFS simulation is
+# +- rate * (relative variance)^{1/2}
+relvar = np.sum((1.0 - probs)/ (probs*fired))
+
+# write all data out to file
+# see also 'allinterfaceseff.out', which includes pruning.
 sname = 'allinterfaces.out'
-# probability of reaching the FINAL interface, starting from lambda1
+# probability of reaching the FINAL interface, starting from lambda0
 prob = np.product(probs)
 # rate of forming FINAL PHASE, starting from phase A
 rate = flux*prob
 fout = open(sname,'w')
-fout.write('SUMMARY\nCritint Critn\n%d %d\nFlux Prob Rate ln(Rate)\n%.6e %.6e %.6e %.6e\n'
-           %(critint,critop,flux,prob,rate,np.log(rate)))
-fout.write('----------\nDETAILED BREAKDOWN\n' \
-           'phase A OP: %d lambda0 OP: %d phase B OP: %d\n' \
-           '%d shots fired from A to lambda0\n' \
+fout.write('SUMMARY\nCritint Critn\n%d %d\n'
+           'Flux Prob Rate ln(Rate) relvar\n%.6e %.6e %.6e %.6e %.6e\n'
+           %(critint,critop,flux,prob,rate,np.log(rate),relvar))
+fout.write('----------\nDETAILED BREAKDOWN\n'
+           'phase A OP: %d lambda0 OP: %d phase B OP: %d\n'
+           '%d shots fired from A to lambda0\n'
            'interface OP fired success P(success) P(finalphase)\n'
-           %(params['lambdaA'],params['lambdas'][0],params['lambdas'][-1],len(ts)))
+           %(params['lambdaA'],params['lambdas'][0],
+             params['lambdas'][-1],len(ts)))
 fstr = ''
 for i in range(nint):
-    fstr = '%s%d->%d %d %d %d %.6f %.6e\n'\
-           %(fstr,i,i+1,params['lambdas'][i+1],fired[i],success[i],probs[i],cumprobs[i])
+    fstr = ('%s%d->%d %d %d %d %.6f %.6e\n'
+            %(fstr,i,i+1,params['lambdas'][i+1],fired[i],
+              success[i],probs[i],cumprobs[i]))
 fout.write(fstr)
 fout.close()
 
 # write all data out to file for pruning run
 sname = 'allinterfaceseff.out'
-# probability of reaching the FINAL interface, starting from lambda1
+# probability of reaching the FINAL interface, starting from lambda0
 probeff = np.product(probseff)
 # rate of forming FINAL phase, starting from phase A
 rateeff = flux*probeff
 fout = open(sname,'w')
-fout.write('SUMMARY\nCritint Critn\n%d %d\nFlux Prob Rate ln(Rate)\n%.6e %.6e %.6e %.6e\n'
+fout.write('SUMMARY\nCritint Critn\n%d %d\nFlux Prob Rate ln(Rate)\n'
+           '%.6e %.6e %.6e %.6e\n'
            %(critinteff,critopeff,flux,probeff,rateeff,np.log(rateeff)))
-fout.write('----------\nDETAILED BREAKDOWN\n' \
-           'phase A OP: %d lambda0 OP: %d phase B OP: %d\n' \
-           '%d shots fired from A to lambda0\ninterface '\
+fout.write('----------\nDETAILED BREAKDOWN\n'
+           'phase A OP: %d lambda0 OP: %d phase B OP: %d\n'
+           '%d shots fired from A to lambda0\ninterface '
            'OP fired success P(success) P(finalphase)\n'
-           %(params['lambdaA'],params['lambdas'][0],params['lambdas'][-1],len(ts)))
+           %(params['lambdaA'],params['lambdas'][0],
+             params['lambdas'][-1],len(ts)))
 fstr = ''
 for i in range(nint):
     fstr = '%s%d->%d %d %d %d %.6f %.6e\n'\
-           %(fstr,i,i+1,params['lambdas'][i+1],firedeff[i],successeff[i],probseff[i],cumprobseff[i])
+           %(fstr,i,i+1,params['lambdas'][i+1],firedeff[i],
+             successeff[i],probseff[i],cumprobseff[i])
 fout.write(fstr)
 fout.close()
