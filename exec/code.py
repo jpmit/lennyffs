d21ece7f0c4f35610350ed0c47ae24323d98ddf2
James Mithen <j.mithen@surrey.ac.uk>
2013-09-12 12:00:37 +0100

    made main executable more OO

--- exec/code.py
+++ exec/code.py
@@ -2,12 +2,15 @@
 # code.py
 # James Mithen
 # j.mithen@surrey.ac.uk
-#
-# MC code for investigating epitaxial nucleation on surfaces.  This
-# code just performs a certain number of MC steps governed by the
-# parameters in the 'in' file.  Note there is no 'Order Parameter'
-# calculation done here.  Thus, this script is mainly useful for
-# equilibration/quenching etc.  For FFS simulations, see codeffs.py
+
+"""
+This is the main Monte-Carlo (MC) code.  This code just performs a
+certain number of MC cycles, governed by the parameters in the 'in'
+file.  It will calculate the 'order parameter' every so often, and
+save the configuration every so often (for both of these, how often is
+specified in the input file. For Forward Flux Sampling (FFS)
+simulations, see codeffs.py.
+"""
 
 import numpy as np
 import funcselector
@@ -18,60 +21,83 @@ import mccycle
 import time
 import sys
 
-# read input parameters and write to file
-params = initsim.getparams()
-writeoutput.writepickparams(params)
-writeoutput.writeparams(params)
+class MCProgram(object):
+    """The main MC program."""
+    
+    def __init__(self):
+        """Read parameters, get initial positions, compute initial
+        potential energy."""
 
-# initialize positions
-positions = initsim.initpositions(params)
+        # read input parameters and write to file
+        self.params = initsim.getparams()
+        writeoutput.writepickparams(params)
+        writeoutput.writeparams(params)
 
-# write initial positions to file if new simulation
-if params['simulation'] == 'new':
-    writeoutput.writexyzld('initpositions.xyz', positions, params)
+        # initialize positions
+        self.positions = initsim.initpositions(params)
 
-# from parameters file, create FuncSelector object.  This will handle
-# correct selection of the underlying fortran/C++ functions correctly
-# (the functions called depend on the potential, i.e. the value of
-# params['potential'], and also on the type of MC cycle wanted, i.e.
-# params['mctype']
-funcman = funcselector.FuncSelector(params)
-totalenergy = funcman.TotalEnergyFunc()
-runcycle = funcman.MCCycleFunc()
-orderp = funcman.OrderParamFunc()
+        # write initial positions to file if new simulation
+        if self.params['simulation'] == 'new':
+            writeoutput.writexyzld('initpositions.xyz', positions,
+                                   self.params)
 
-# number of times to call MC cycle function
-ncall = int( np.ceil(params['ncycle'] / float(params['opsamp'])) )
+        # From params dictionary create FuncSelector object.  This
+        # will handle correct selection of the underlying fortran/C++
+        # functions correctly (the functions called depend on the
+        # potential, i.e. the value of params['potential'], and also
+        # on the type of MC cycle wanted, i.e.  params['mctype'], and
+        # on the order parameter desired, params['orderparam'].
+        funcman = funcselector.FuncSelector(params)
+        self.totalenergy = funcman.TotalEnergyFunc()
+        self.runcycle = funcman.MCCycleFunc()
+        self.orderp = funcman.OrderParamFunc()
 
-# number of cycles each time we call MC cycle function
-params['cycle'] = min(params['ncycle'], params['opsamp'])
+        # number of times to call MC cycle function
+        self.ncall = int( np.ceil(params['ncycle'] /
+                                  float(params['opsamp'])) )
 
-# compute initial energy
-epot = totalenergy(positions,params)
+        # number of cycles each time we call MC cycle function
+        self.params['cycle'] = min(self.params['ncycle'],
+                                   self.params['opsamp'])
 
-# perform MC simulation
-opfile = open('opval.out','w')
-starttime = time.time()
+        def run(self):
+            """Perform the MC simulation."""
 
-# run the MC cycles
-cyclesdone = 0
-opfile.write('{0} {1}\n'.format(0, orderp(positions, params)))
-for cy in range(ncall):
-    positions, epot = runcycle(positions, params, epot)
-    cyclesdone += params['cycle']
-    # write out order parameter
-    opfile.write('{0} {1}\n'.format(cyclesdone, orderp(positions,
-                                                       params)))
-    opfile.flush()
-    # write out pos file if required
-    if (cyclesdone % params['nsave'] == 0):
-        writeoutput.writexyzld('pos{0}.xyz'.format(cy), positions,
-                               params)
+            # compute initial energy
+            epot = totalenergy(self.positions, self.params)
+
+            # file for writing order parameter
+            opfile = open('opval.out','w')
+            starttime = time.time()
+
+            # run the MC cycles
+            cyclesdone = 0
+            opfile.write('{0} {1}\n'.format(0, orderp(self.positions,
+                                                      self.params)))
+            for cy in range(ncall):
+                self.positions, epot = self.runcycle(self.positions,
+                                                     self.params,
+                                                     self.epot)
+                cyclesdone += self.params['cycle']
+                # write out order parameter
+                opfile.write('{0} {1}\n'.format(cyclesdone,
+                                                self.orderp(self.positions,
+                                                            self.params)))
+                opfile.flush()
+                # write out pos file if required
+                if (cyclesdone % self.params['nsave'] == 0):
+                    writeoutput.writexyzld('pos{0}.xyz'.format(cy), self.positions,
+                                           self.params)
         
-endtime = time.time()
+        endtime = time.time()
 
-# write final positions to file
-writeoutput.writexyzld('finalpositions.xyz', positions, params)
+        # write final positions to file
+        writeoutput.writexyzld('finalpositions.xyz', self.positions,
+                               self.params)
 
-# write runtime to stderr
-sys.stderr.write("runtime in s: %.3f\n" %(endtime - starttime))
+        # write runtime to stderr
+        sys.stderr.write("runtime in s: %.3f\n" %(endtime - starttime))
+
+if __name__ == '__main':
+    mcprog = MCProgram()
+    mcprog.run()
