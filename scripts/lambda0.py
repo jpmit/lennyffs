77eb4d4e439ba15c73ad84a0528e2cc38b73f2df
James Mithen <j.mithen@surrey.ac.uk>
2014-01-15 16:32:32 +0000

    fix FFS

--- scripts/lambda0.py
+++ scripts/lambda0.py
@@ -2,9 +2,11 @@
 # lambda0.py
 # James Mithen
 # j.mithen@surrey.ac.uk
-#
-# Take the system from phase A to Lambda0 in FFS simulation.
-# see Allen, Valerani, ten Wolde J. Phys. Condens. matter 21, 463102
+
+"""
+Take the system from phase A to Lambda0 in FFS simulation.  see Allen,
+ Valerani, ten Wolde J. Phys. Condens. matter 21, 463102.
+"""
 
 import sys
 import pickle
@@ -12,9 +14,7 @@ import numpy as np
 import initsim
 import energy
 import mccycle
-from bops import bopxbulk
 from ffsfunctions import savelambda0config, getpickparams
-from writeoutput import  writexyz
 import potselector
 from lenexceptions import *
 
@@ -24,29 +24,29 @@ lamA = params['lambdaA']
 lam0 = params['lambdas'][0]
 totalqhits = params['totalqhits'] # num times to go through lambda0
                                   # from phase A
-lamsamp = params['lambdasamp'] # number of MC cycles per OP evaluation
+lamsamp = params['lambdasamp']    # num MC cycles per OP evaluation
 params['cycle'] = lamsamp
 
 # initialize positions
 positions = initsim.initpositions(params)
 
-# check that lambda < lamA (we are in phase A)
-nxtal, bopx = bopxbulk(positions,params)
-if (bopx >= lamA):
-    raise FFSError, ('BOPxbulk is {0}, system must start in phase A, '
-                     'BOPxbulk < {1}'.format(bopx,lamA))
-
 # get the correct energy function and MC cycle function using
 # PotSelector interface
+potman = potselector.PotSelector(params)
+totalenergyfunc = potman.TotalEnergyFunc()
+cyclefunc = potman.MCCycleFunc()
+orderpfunc = potman.OrderParamFunc()
 
-PotManager = potselector.PotSelector(params)
-totalenergyfunc = PotManager.TotalEnergyFunc()
-cyclefunc = PotManager.MCCycleFunc()
+# check that lambda < lamA (we are in phase A)
+op = orderpfunc(positions,params)
+if (op >= lamA):
+    raise FFSError, ('OP is {0}, system must start in phase A, '
+                     'OP < {1}'.format(op, lamA))
 
 # write initial OP to file
-fin = open('ffsacc.out','w')
-fin.write('ncycles nxtal bopX\n')
-fin.write('%d %d %d\n' %(0,nxtal,bopx))
+fin = open('ffsacc.out', 'w')
+fin.write('ncycles OP\n')
+fin.write('{0} {1}\n'.format(0, op))
 fin.flush()
 
 # start simulation
@@ -54,48 +54,55 @@ qhits = 0 # num times passed through lambda0
 thit = 0
 ttot = 0
 
-epot = totalenergyfunc(positions,params)    
+# initial potential energy
+epot = totalenergyfunc(positions, params)
+
+# evolve the system in time until it has 'hit' the interface (lambda0)
+# the desired number of times.
 while (qhits < totalqhits):
-    # make some trial moves
-    positions,epot = cyclefunc(positions,params,epot)
+    # go forward in time
+    # TODO: make this work for MD (will require velocities and forces)
+    positions, epot = cyclefunc(positions, params, epot)
     thit = thit + lamsamp
     ttot = ttot + lamsamp
     # evaluate OP
-    nxtal,bopx = bopxbulk(positions,params)
+    op = orderpfunc(positions, params)
     # write OP and total time to file
-    fin.write('%d %d %d\n' %(ttot,nxtal,bopx))
+    fin.write('{0} {1} {2}\n'.format(ttot, nxtal, bopx))
     fin.flush()
     if (bopx >= lam0):
         # we hit the interface
         qhits = qhits + 1
-        savelambda0config(qhits,thit,positions,params)
+        savelambda0config(qhits, thit, positions, params)
         thit = 0
         # now let the system relax back to lambda A
         while (bopx >= lamA):
-            positions,epot = cyclefunc(positions,params,epot)
+            # TODO: make this work for MD (will require velocities and forces)
+            positions, epot = cyclefunc(positions, params, epot)
             ttot = ttot + lamsamp                
             # evaluate OP
-            nxtal, bopx = bopxbulk(positions,params)
+            op = orderpfunc(positions, params)
             # write OP and total time to file
-            fin.write('%d %d %d\n' %(ttot,nxtal,bopx))
-            # if we reached lam0*10 (arbitrary, means going to phase B),
-            # then return to phase A
+            fin.write('{0} {1} {2}\n'.format(ttot, nxtal, bopx))
+            # if we reached lam0*10 then return to phase A by
+            # reloading initial positions. lam0*10 is arbitrary, the
+            # aim here is to stop the system from reaching phase B.
             if (bopx > lam0*10):
                 positions = initsim.initpositions(params)
-                nxtal,bopx = bopxbulk(positions,params)
-                epot = totalenergyfunc(positions,params)
+                op = orderpfunc(positions, params)
+                epot = totalenergyfunc(positions, params)
                 fin.write('RETURNING TO PHASE A\n')
-                fin.write('%d %d %d\n' %(ttot,nxtal,bopx))
+                fin.write('{0} {1} {2}\n'.format(ttot, nxtal, bopx))
                 
 fin.close()
 
-# now create the dictionary with shot information
-# this is pickled and read by shots at the subsequent interface
-# (see takeshot.py)
+# now create the dictionary with shot information.  This is pickled
+# and read by shots at the subsequent interface (see takeshot.py).
 shotdict = {'nshots': totalqhits,'nshotseff': totalqhits,
             'nsuccess': totalqhits,'nsuccesseff' : totalqhits,
             'successnumbers' : np.array(range(1,totalqhits+1)),
             'successweights': np.ones(totalqhits)}
+
 # write out to pickle file
 fout = open('interface0.pkl', 'wb')
 pickle.dump(shotdict, fout)
