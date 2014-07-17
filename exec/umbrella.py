581cdfff47356d31b8bc33a25144fc134e9310c9
Adam Callison <ac00285@phws27.ph.surrey.ac.uk>
2014-07-17 15:11:18 +0100

    Added support for optional indexing of output files via command line -w argument

--- exec/umbrella.py
+++ exec/umbrella.py
@@ -4,12 +4,14 @@
 # j.mithen@surrey.ac.uk
 
 """
-This is the main Monte-Carlo (MC) code.  This code just performs a
-certain number of MC cycles, governed by the parameters in the 'in'
-file.  It will calculate the 'order parameter' every so often, and
-save the configuration every so often (for both of these, how often is
-specified in the input file). For Forward Flux Sampling (FFS)
-simulations, see codeffs.py.
+This is the umbrella-enabled main Monte-Carlo (MC) code.
+This code just performs a certain number of MC cycles, governed by the
+parameters in the 'in' file.  It will calculate the 'order parameter'
+every so often, and save the configuration every so often (for both of these,
+how often is specified in the input file). Makes use of umbrella sampling
+
+Run with -w [index] for optional indexed output files. Index can be
+anything
 """
 
 import numpy as np
@@ -29,16 +31,21 @@ class MProgram(object):
     
     def __init__(self):
         """
-        Read parameters, get initial positions (and velocities if MD),
+        Read parameters, get initial positions,
         and setup any parameters needed for the simulation.
         """
-
+        #allow taking index as command line argument
+        if len(sys.argv) != 1 and sys.argv[1] == '-w':
+            self.iwind = sys.argv[2]
+        else:
+            self.iwind = ''
         # read input parameters and write to file
         self.params = initsim.getparams()
-        # pickled version 'params.pkl'
-        writeoutput.writepickparams(self.params)
-        # human readable version 'params.out'
-        writeoutput.writeparams(self.params)
+        if self.iwind == '':  #indexed pkl files wil already exist otherwise
+            # pickled version 'params.pkl'
+            writeoutput.writepickparams(self.params)
+            # human readable version 'params.out'
+            writeoutput.writeparams(self.params)
 
         # From params dictionary create FuncSelector object.  This
         # will handle correct selection of the underlying fortran/C++
@@ -52,30 +59,13 @@ class MProgram(object):
         self.orderp = funcman.OrderParamFunc()
         self.writexyz = funcman.WriteXyzFunc()
 
-        # initialize positions (and velocities and forces if we are
-        # doing MD rather than MC).
-        if self.params['mctype'] == 'md':
-            self.positions, \
-            self.velocities = initsim.\
-            initpositionsvelocities(self.params)
-            # Note we only have MD implemented for Gaussian potential
-            # at present; the force function should in principle be
-            # handled by the FuncSelector interface.
-            self.forces = force.gauss_forceslist(self.positions,
-                                                 self.params)            
-        else:
-            # MC simulation.  We initialize positions only.
-            self.positions = initsim.initpositions(self.params)
+        # initialize positions 
+        self.positions = initsim.initpositions(self.params)
 
         # write initial positions to file if new simulation
         if self.params['simulation'] == 'new':
-            self.writexyz('initpositions.xyz', self.positions,
+            self.writexyz('initpositions{0}.xyz'.format(self.iwind), self.positions,
                           self.params)
-            # write initial pickle file that stores both positions and
-            # velocities if we are doing an MD simulation.
-            if self.params['mctype'] == 'md':
-                writeoutput.writemdpick('initpositions.pkl',
-                                        self.positions, self.velocities)
 
         # number of times to call MC cycle function
         self.numbrellacycles = self.params['numbrellacycles']
@@ -85,56 +75,8 @@ class MProgram(object):
         self.params['cycle'] = self.params['nunbiased']
 
     def run(self):
-        """Perform the MC/MD simulation."""
-
-        if self.params['mctype'] == 'md':
-            self.run_md()
-        else:
-            self.run_mc()
-
-    def run_md(self):
-        """
-        Perform the MD simulation.
-
-        TODO: integrate this into the run_mc method (?)
-        """
-
-        # file for writing order parameter
-        opfile = open('opval.out','w')
-
-        # run the MC cycles
-        cyclesdone = 0
-        opfile.write('{0} {1}\n'.format(0, self.orderp(self.positions,
-                                                       self.params)))
-        starttime = time.time()
-
-        for cy in range(self.ncall):
-            self.positions, self.velocities,\
-            self.forces = self.runcycle(self.positions,
-                                        self.params, self.velocities,
-                                        self.forces)
-            
-            cyclesdone += self.params['cycle']
-            # write out order parameter
-            opfile.write('{0} {1}\n'.format(cyclesdone,
-                                            self.orderp(self.positions,
-                                                        self.params)))
-            opfile.flush()
-            # write out pos file if required
-            if (cyclesdone % self.params['nsave'] == 0):
-                self.writexyz('pos{0}.xyz'.format(cy), self.positions,
-                              self.params)
-
-        endtime = time.time()
-
-        # write final positions to file
-        self.writexyz('finalpositions.xyz', self.positions, self.params)
-
-        # write pickled save file which includes the velocities
-        writeoutput.writemdpick('finalpositions.pkl', self.positions, self.velocities)
-
-        # write runtime to stderr
-        sys.stderr.write("runtime in s: {:.3f}\n".format(endtime - starttime))
+        """Perform the MC simulation."""
+        self.run_mc()
 
     def run_mc(self):
         """Perform the MC simulation."""
@@ -143,9 +85,9 @@ class MProgram(object):
         epot = self.totalenergy(self.positions, self.params)
 
         # file for writing order parameter
-        opfile = open('opval.out','w')
+        opfile = open('opval{0}.out'.format(self.iwind),'w')
 
-        # run the MC cycles
+        # write time 0 and initial orderp
         cyclesdone = 0
         opfile.write('{0} {1}\n'.format(0, self.orderp(self.positions,
                                                        self.params)))
@@ -191,13 +133,13 @@ class MProgram(object):
             opfile.flush()
             # write out pos file if required
             if (cyclesdone % self.params['nsave'] == 0):
-                self.writexyz('pos{0}.xyz'.format(cyclesdone), self.positions,
+                self.writexyz('{0}pos{1}.xyz'.format(self.iwind,cyclesdone), self.positions,
                               self.params)
 
         endtime = time.time()
 
         # write final positions to file
-        self.writexyz('finalpositions.xyz', self.positions, self.params)
+        self.writexyz('finalpositions{0}.xyz'.format(self.iwind), self.positions, self.params)
 
         # write runtime to stderr
         sys.stderr.write("runtime in s: {:.3f}\n".format(endtime - starttime))
@@ -205,7 +147,8 @@ class MProgram(object):
         # if we were npt, print new box volume
         if self.params['mctype'] == 'npt':
             sys.stderr.write('new box volume: {0}\n'\
-                             .format(self.params['lboxx']*\
+                             .format(self.iwind,
+                                     self.params['lboxx']*\
                                      self.params['lboxy']*\
                                      self.params['lboxz']))
 
